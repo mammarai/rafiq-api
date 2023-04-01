c5801ed6adac53aa6c9ebbd2716c6054cb41fac4
Muneeb ur Rahman <muneebdev1@gmail.com>
2023-04-02 03:25:47 +0500

    better pdf text extraction

--- src/controllers/chatbots.ts
+++ src/controllers/chatbots.ts
@@ -3,6 +3,7 @@ import { get } from "lodash";
 import { Crawler, Page } from "../helpers/crawler";
 import { newChatbot } from "../db/chatbots";
 import pdfReader from "../helpers/pdfReader";
+import { extractTextFromPDF } from "../helpers/pdf";
 
 export const createChatbot = async (
   req: express.Request,
@@ -13,16 +14,17 @@ export const createChatbot = async (
     // if (!name || !documents) {
     //   return res.status(400).send({ error: "missing required fields." });
     // }
-    const pdfFile =
-      (req.files as { [fieldname: string]: Express.Multer.File[] })[
-        "pdf"
-      ]?.[0];
-      const urls = req.body.urls?.split(",");
+    const pdfFile = (
+      req.files as { [fieldname: string]: Express.Multer.File[] }
+    )["pdf"]?.[0];
+    const urls = req.body.urls?.split(",");
     // const id = (get(req, "user.uid") as string).toString();
     // const crawler = new Crawler(documents, 2, 200);
     // const pages = (await crawler.start()) as Page[];
     //const text = await pdfReader.getPDFText(pdfFile);
-    return res.status(200).send();
+    const text2 = await extractTextFromPDF(pdfFile);
+    console.log(text2);
+    return res.status(200).send(text2);
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
