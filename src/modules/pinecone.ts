37d109706bd0a6ba358f5f09b36b66d5ef4241b0
Muneeb ur Rahman <muneebdev1@gmail.com>
2023-04-06 01:16:00 +0500

    pinecone.ts

---
+++ src/modules/pinecone.ts
@@ -0,0 +1,10 @@
+import { PineconeClient } from "@pinecone-database/pinecone";
+
+const client = new PineconeClient();
+
+await client.init({
+  apiKey: process.env.PINECONE_API_KEY!,
+  environment: process.env.PINECONE_ENVIRONMENT!,
+});
+
+export default client;
